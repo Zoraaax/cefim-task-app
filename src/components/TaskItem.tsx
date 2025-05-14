57deb1e5fa155ec5790aca4f292b78ebc7918ac1
Valentin Thibault <valentin.thlt@gmail.com>
2025-05-14 16:10:23 +0200

    feat: add delete and status change

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,16 +1,25 @@
 import type { Task } from "../types/TaskType"
+import { FaTrash } from "react-icons/fa"
 
 type TaskItemProps = {
     task: Task
+    onDelete: (id: number) => void
+    onStatusChange: (id: number, status: Task["status"]) => void
 }
 
-export const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
+export const TaskItem: React.FC<TaskItemProps> = ({ task, onDelete, onStatusChange }) => {
     return (
         <div key={task.id} className="mb-4 p-4 border rounded shadow">
             <h3 className="text-xl font-bold">{task.title}</h3>
             <p className="mt-2">{task.description}</p>
             <p className="mt-2">Status: {task.status}</p>
             <p className="mt-2">Date de fin: {new Date(task.endDate).toLocaleDateString()}</p>
+            <button onClick={() => onDelete(task.id)}><FaTrash /></button>
+            <select value={task.status} onChange={(e) => onStatusChange(task.id, e.target.value as Task["status"])}>
+                <option value="En attente">En attente</option>
+                <option value="En cours">En cours</option>
+                <option value="Terminée">Terminée</option>
+            </select>
         </div>
     )
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,13 +3,35 @@ import type { Task } from "../types/TaskType"
 import { TaskItem } from "./TaskItem"
 
 export const TaskList: React.FC = () => {
-    const [tasks] = useLocalStorage<Task[]>("tasks", [])
+    const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", [])
+
+    const handleDelete = (id: number) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id)
+        setTasks(updatedTasks)
+    }
+
+    const handleStatusChange = (id: number, status: Task["status"]) => {
+        const updatedTask = tasks.map((task) => {
+            if (task.id === id) {
+                return { ...task, status}
+            }
+            return task
+        })
+        setTasks(updatedTask)
+    }
+
+    console.log(tasks)
 
     return (
         <div>
             <h2>Liste des tâches</h2>
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
+                <TaskItem 
+                    key={task.id} 
+                    task={task} 
+                    onDelete={handleDelete} 
+                    onStatusChange={handleStatusChange} 
+                />
             ))}
         </div>
     )
