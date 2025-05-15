d6461a0c7a44f0ba30285132e405d9a20a0b06ca
Valentin Thibault <valentin.thlt@gmail.com>
2025-05-15 13:59:00 +0200

    feat: Add task filter by status

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import { useLocalStorage } from "../hooks/useLocalStorage"
 import type { Task } from "../types/TaskType"
 import { TaskItem } from "./TaskItem"
 
 export const TaskList: React.FC = () => {
     const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", [])
+    const [statusFilter, setStatusFilter] = useState<Task["status"] | "all">("all")
 
     const handleDelete = (id: number) => {
         const updatedTasks = tasks.filter((task) => task.id !== id)
@@ -20,12 +22,20 @@ export const TaskList: React.FC = () => {
         setTasks(updatedTask)
     }
 
-    console.log(tasks)
+    const filteredTasks = statusFilter === "all" 
+        ? tasks 
+        : tasks.filter((task) => task.status === statusFilter)
 
     return (
         <div>
             <h2>Liste des tâches</h2>
-            {tasks.map((task) => (
+            <div>
+                <button onClick={() => setStatusFilter("all")}>Toutes</button>
+                <button onClick={() => setStatusFilter("En attente")}>En attente</button>
+                <button onClick={() => setStatusFilter("En cours")}>En cours</button>
+                <button onClick={() => setStatusFilter("Terminée")}>Terminée</button>
+            </div>
+            {filteredTasks.map((task) => (
                 <TaskItem 
                     key={task.id} 
                     task={task} 
